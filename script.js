1a53cf863b190eb54be600a576a83023cf26fcd7
nitinkhatri4 <nitinkhatri1764@gmail.com>
2025-03-30 20:01:52 +0530

    path-changes-again

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const apiKey = "63ab886af70758895dcc9117fc860c59";
 const apiUrl = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=`;
 const cityName = document.querySelector(".search-box input");
 const submitBtn = document.querySelector(".search-box button");
+const weatherIcon = document.querySelector(".weather-icon");
 
 const checkWeather = async (city) => {
   const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
@@ -17,10 +18,20 @@ const checkWeather = async (city) => {
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
     let skyCondition = data.weather[0].main;
-    document.querySelector(
-      ".weather-icon"
-    ).src = `images/${skyCondition}.png`;
-    document.querySelector(".weather-icon").alt = skyCondition;
+    if (skyCondition == "Clear") {
+      weatherIcon.src = "./images/clear.png";
+    } else if (skyCondition == "Clouds") {
+      weatherIcon.src = "./images/clouds.png";
+    } else if (skyCondition == "Drizzle") {
+      weatherIcon.src = "./images/drizzle.png";
+    } else if (skyCondition == "Mist") {
+      weatherIcon.src = "./images/mist.png";
+    } else if (skyCondition == "Rain") {
+      weatherIcon.src = "./images/rain.png";
+    } else if (skyCondition == "Snow") {
+      weatherIcon.src = "./images/snow.png";
+    }
+    weatherIcon.alt = skyCondition;
 
     document.querySelector(".weather").style.display = "block";
     document.querySelector(".error").style.display = "none";
